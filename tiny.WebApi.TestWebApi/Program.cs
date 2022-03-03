615edb40d015faaf89a8a996cda5b709a88d3053
Chetan Mandhania <mnchetan@live.com>
2022-03-04 00:48:19 +0530

    fixing the way to call the service.

--- tiny.WebApi.TestWebApi/Program.cs
+++ tiny.WebApi.TestWebApi/Program.cs
@@ -19,20 +19,20 @@ var filePath = Path.Combine(new FileInfo(Assembly.GetExecutingAssembly().Locatio
 if (File.Exists(filePath))
 {
     var data = JsonConvert.DeserializeObject<Dictionary<string, object>>(tiny.WebApi.Helpers.FileReadWriteHelper.ReadAllText(filePath));
+    var url = data.GetValueOrDefault<string, object>("urls");
+    if (args is not null && args.Length > 0)
+        for (int i = 0; i < args.Length; i++)
+        {
+            var param = args[i];
+            if (param == "--urls")
+                if (args.Length > i + 1)
+                {
+                    url = args[i + 1];
+                    break;
+                }
+        }
     if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
     {
-        var url = data.GetValueOrDefault<string, object>("urls");
-        if (args is not null && args.Length > 0)
-            for (int i = 0; i < args.Length; i++)
-            {
-                var param = args[i];
-                if (param == "--urls")
-                    if (args.Length > i + 1)
-                    {
-                        url = args[i + 1];
-                        break;
-                    }
-            }
         if (IsRunningInProcessIIS)
         {
             Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((h, c) =>
@@ -49,38 +49,35 @@ if (File.Exists(filePath))
             {
                 case not null when args.Length > 0:
                     {
-                        Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(wb =>
+                        Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((hbc, icb) =>
+                        {
+                            icb.Build();
+                        }).ConfigureWebHostDefaults(wb =>
                         {
                             wb.UseUrls(url is string ? (url as string) : "http://localhost:5000");
+                            wb.UseContentRoot(new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName);
                             wb.UseStartup<Startup>();
                         }).Build().Run();
                         break;
                     }
-#pragma warning disable CS8602 // Dereference of a possibly null reference.
-                case null when args.Length == 0:
-#pragma warning restore CS8602 // Dereference of a possibly null reference.
-                    Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((hbc, icb) =>
-                    {
-                        icb.Build();
-                    }).ConfigureWebHostDefaults(wb =>
-                    {
-                        wb.UseUrls(url is string ? (url as string) : "http://localhost:5000");
-                        wb.UseStartup<Startup>();
-                    }).UseWindowsService();
-                    break;
+
                 default:
-                    Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((h, c) =>
                     {
-                        c.Build();
-                    }).ConfigureWebHostDefaults(w =>
-                    {
-                        w.UseStartup<Startup>();
-                    }).Build().Run();
-                    break;
+                        Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((hbc, icb) =>
+                        {
+                            icb.Build();
+                        }).ConfigureWebHostDefaults(wb =>
+                        {
+                            wb.UseUrls(url is string ? (url as string) : "http://localhost:5000");
+                            wb.UseContentRoot(new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName);
+                            wb.UseStartup<Startup>();
+                        }).Build().Run();
+                        break;
+                    }
             }
         }
     }
-    else Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(wb => wb.UseStartup<Startup>()).Build().Run();
+    else Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(wb => { wb.UseUrls(url is string ? (url as string) : "http://localhost:5000"); wb.UseStartup<Startup>(); }).Build().Run();
 }
 else Environment.FailFast($"Missing appsettings.json file at path : {filePath} and hence unable to start tiny.WebApi.");
 #pragma warning restore CS8604 // Possible null reference argument.
